ccb73193ebe99095a6e7bfff744c010d6fecb238
bizarf <snesmaster40@googlemail.com>
2023-01-27 17:49:47 +0000

    Cleaned up the code a bit

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ let table = document.querySelector("#library-table");
 let bookForm = document.querySelector("#addBookForm");
 let bookTableBody = document.querySelector("#book-data");
 
-// the constructor...
-// function Book(title, author, pages, read, id) {
-//     this.title = title
-//     this.author = author
-//     this.pages = pages
-//     this.read = read
-//     this.id = id
-//     myLibrary.push(this);
-// }
-
 class Book {
     constructor(title, author, pages, read, id) {
         this.title = title;
@@ -26,17 +16,16 @@ class Book {
 
 // adds new books to the library array
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read)
-    // return myLibrary.push(newBook)
+    new Book(title, author, pages, read);
 }
 
 // submit button events
 bookForm.addEventListener("submit", function (e) {
-    e.preventDefault()
-    addBookToLibrary(title.value, author.value, pages.value, read.checked)
-    libraryDisplay()
-    clearInputs()
-})
+    e.preventDefault();
+    addBookToLibrary(title.value, author.value, pages.value, read.checked);
+    libraryDisplay();
+    clearInputs();
+});
 
 // Resets the form
 function clearInputs() {
@@ -45,73 +34,54 @@ function clearInputs() {
 
 // creates new table rows
 function addTableRow(title, author, pages, read, id) {
-    let deleteTd = document.createElement("button")
-    let statusBtn = document.createElement("button")
-    deleteTd.textContent = "Delete"
-    deleteTd.classList = "deleteBtn"
-    deleteTd.dataset.id = id
-    let newRow = bookTableBody.insertRow()
-    let titleCell = newRow.insertCell()
-    let authorCell = newRow.insertCell(1)
-    let pagesCell = newRow.insertCell(2)
-    let readCell = newRow.insertCell(3)
-    let deleteBtnCell = newRow.insertCell(4)
-    pagesCell.classList = "pagesCol"
-    readCell.classList = "bookStatusCol"
-    deleteBtnCell.classList = "deleteBtnCol"
-    titleCell.textContent = title
-    authorCell.textContent = author
-    pagesCell.textContent = pages
-    statusBtn.textContent = read
-    statusBtn.classList = "bookStatus"
-    statusBtn.dataset.statusId = id
-    readCell.appendChild(statusBtn)
-    deleteBtnCell.appendChild(deleteTd)
+    let deleteTd = document.createElement("button");
+    let statusBtn = document.createElement("button");
+    deleteTd.textContent = "Delete";
+    deleteTd.classList = "deleteBtn";
+    deleteTd.dataset.id = id;
+    let newRow = bookTableBody.insertRow();
+    let titleCell = newRow.insertCell();
+    let authorCell = newRow.insertCell(1);
+    let pagesCell = newRow.insertCell(2);
+    let readCell = newRow.insertCell(3);
+    let deleteBtnCell = newRow.insertCell(4);
+    pagesCell.classList = "pagesCol";
+    readCell.classList = "bookStatusCol";
+    deleteBtnCell.classList = "deleteBtnCol";
+    titleCell.textContent = title;
+    authorCell.textContent = author;
+    pagesCell.textContent = pages;
+    statusBtn.textContent = read;
+    statusBtn.classList = "bookStatus";
+    statusBtn.dataset.statusId = id;
+    readCell.appendChild(statusBtn);
+    deleteBtnCell.appendChild(deleteTd);
 }
 
 // test data
-addBookToLibrary(
-    "Harry Potter and the Philosopher's Stone",
-    "J. K. Rowling",
-    223,
-    true
-);
-
-addBookToLibrary(
-    "The Hobbit",
-    "J. R. R. Tolkien",
-    310,
-    false
-);
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 310, false);
 
 // Display function populates the table
 function libraryDisplay() {
     bookTableBody.textContent = "";
     bookId();
-    for (let book of myLibrary) {
+    myLibrary.map((book) =>
         addTableRow(book.title, book.author, book.pages, book.read, book.id)
-    }
+    );
     deleteBtn();
     setReadStatusClass();
     bookStatusToggle();
 }
-libraryDisplay()
-
-// Clears the table except for the headers
-function clearTableRows() {
-    while (table.rows.length > 1) {
-        table.deleteRow(1)
-    }
-}
+libraryDisplay();
 
 // sets the labels of the book status
 function setReadStatusClass() {
     let tableCell = document.querySelectorAll(".bookStatus");
     for (let cell of tableCell) {
         if (cell.textContent === "false") {
-            cell.textContent = "Unread"
+            cell.textContent = "Unread";
         } else if (cell.textContent === "true") {
-            cell.textContent = "Read"
+            cell.textContent = "Read";
         }
     }
 }
@@ -120,29 +90,30 @@ function setReadStatusClass() {
 function bookId() {
     myLibrary.forEach(() => {
         for (let i = 0; i < myLibrary.length; i++) {
-            myLibrary[i].id = i
+            myLibrary[i].id = i;
         }
-    })
+    });
 }
 
 // function for the delete buttons. uses data attribute which is tied to array index
 function deleteBtn() {
-    let bookDeleteBtn = document.querySelectorAll('[data-id]')
-    bookDeleteBtn.forEach(bookDelete => {
+    let bookDeleteBtn = document.querySelectorAll("[data-id]");
+    bookDeleteBtn.forEach((bookDelete) => {
         bookDelete.addEventListener("click", () => {
-            myLibrary.splice(bookDelete.dataset.id, 1)
-            libraryDisplay()
-        })
-    })
+            myLibrary.splice(bookDelete.dataset.id, 1);
+            libraryDisplay();
+        });
+    });
 }
 
 // function to toggle the book read status
 function bookStatusToggle() {
-    let bStatusBtn = document.querySelectorAll('[data-status-id]')
-    bStatusBtn.forEach(status => {
+    let bStatusBtn = document.querySelectorAll("[data-status-id]");
+    bStatusBtn.forEach((status) => {
         status.addEventListener("click", () => {
-            myLibrary[status.dataset.statusId].read = !myLibrary[status.dataset.statusId].read
-            libraryDisplay()
-        })
-    })
+            myLibrary[status.dataset.statusId].read =
+                !myLibrary[status.dataset.statusId].read;
+            libraryDisplay();
+        });
+    });
 }
